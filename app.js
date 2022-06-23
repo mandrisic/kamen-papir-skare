bd302af017f44db81fdd66685de58e8afff57d56
Mirna Andrišić <mandrisic@ffos.hr>
2022-06-23 20:53:36 +0200

    RWD + SVG
    Responsive web design:
    Mobitel - max 480px
    Tablet - min 481px - 768px
    Računalo - min 769px
    
    SVG:
    Sketchbook i Inkscape
    Inspirirano vintage neon znakovima

--- app.js
+++ app.js
@@ -3,99 +3,99 @@ const game = () => {
   let cScore = 0;
 
   const startGame = () => {
-    const playBtn = document.querySelector(".intro button");
-    const introScreen = document.querySelector(".intro");
-    const match = document.querySelector(".match");
+    const playBtn = document.querySelector('.intro button');
+    const introScreen = document.querySelector('.intro');
+    const match = document.querySelector('.match');
 
-    playBtn.addEventListener("click", () => {
-      introScreen.classList.add("fadeOut");
-      match.classList.add("fadeIn");
+    playBtn.addEventListener('click', () => {
+      introScreen.classList.add('fadeOut');
+      match.classList.add('fadeIn');
     });
   };
 
   const playMatch = () => {
-    const options = document.querySelectorAll(".options button");
-    const playerHand = document.querySelector(".player-hand");
-    const computerHand = document.querySelector(".computer-hand");
-    const hands = document.querySelectorAll(".hands img");
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+    const hands = document.querySelectorAll('.hands img');
 
     hands.forEach(hand => {
-      hand.addEventListener("animationend", function() {
-        this.style.animation = "";
+      hand.addEventListener('animationend', function() {
+        this.style.animation = '';
       });
     });
 
-    const computerOptions = ["kamen", "papir", "škare"];
+    const computerOptions = ['kamen', 'papir', 'skare'];
 
     options.forEach(option => {
-      option.addEventListener("click", function() {
+      option.addEventListener('click', function() {
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
 
         setTimeout(() => {
           compareHands(this.textContent, computerChoice);
 
-          playerHand.src = `./slike/${this.textContent}.png`;
-          computerHand.src = `./slike/${computerChoice}.png`;
+          playerHand.src = `./igrac/${this.textContent}.svg`;
+          computerHand.src = `./comp/${computerChoice}.svg`;
         }, 2000);
 
-        playerHand.style.animation = "shakePlayer 2s ease";
-        computerHand.style.animation = "shakeComputer 2s ease";
+        playerHand.style.animation = 'shakePlayer 2s ease';
+        computerHand.style.animation = 'shakeComputer 2s ease';
       });
     });
   };
 
   const updateScore = () => {
-    const playerScore = document.querySelector(".player-score p");
-    const computerScore = document.querySelector(".computer-score p");
+    const playerScore = document.querySelector('.player-score p');
+    const computerScore = document.querySelector('.computer-score p');
     playerScore.textContent = pScore;
     computerScore.textContent = cScore;
   };
 
   const compareHands = (playerChoice, computerChoice) => {
-    const winner = document.querySelector(".winner");
+    const winner = document.querySelector('.winner');
 
     if (playerChoice === computerChoice) {
-      winner.textContent = "Neriješeno";
+      winner.textContent = 'Neriješeno';
       return;
     }
 
-    if (playerChoice === "kamen") {
-      if (computerChoice === "škare") {
-        winner.textContent = "Igrač pobjeđuje";
+    if (playerChoice === 'kamen') {
+      if (computerChoice === 'skare') {
+        winner.textContent = 'Igrač pobjeđuje';
         pScore++;
         updateScore();
         return;
       } else {
-        winner.textContent = "Računalo pobjeđuje";
+        winner.textContent = 'Računalo pobjeđuje';
         cScore++;
         updateScore();
         return;
       }
     }
 
-    if (playerChoice === "papir") {
-      if (computerChoice === "škare") {
-        winner.textContent = "Računalo pobjeđuje";
+    if (playerChoice === 'papir') {
+      if (computerChoice === 'skare') {
+        winner.textContent = 'Računalo pobjeđuje';
         cScore++;
         updateScore();
         return;
       } else {
-        winner.textContent = "Igrač pobjeđuje";
+        winner.textContent = 'Igrač pobjeđuje';
         pScore++;
         updateScore();
         return;
       }
     }
 
-    if (playerChoice === "škare") {
-      if (computerChoice === "kamen") {
-        winner.textContent = "Računalo pobjeđuje";
+    if (playerChoice === 'skare') {
+      if (computerChoice === 'kamen') {
+        winner.textContent = 'Računalo pobjeđuje';
         cScore++;
         updateScore();
         return;
       } else {
-        winner.textContent = "Igrač pobjeđuje";
+        winner.textContent = 'Igrač pobjeđuje';
         pScore++;
         updateScore();
         return;
